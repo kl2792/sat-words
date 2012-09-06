cab96c456de8fcce6582a1b643248776f2c2e255
Steve Kinney <hello@stevekinney.net>
2012-09-05 21:21:42 -0400

    Initial commit.

---
+++ sat-words.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'csv'
+
+get '/' do
+  csv = CSV.open('words.csv')
+  today = Date.today.strftime('%m/%d/%y')
+  @word = csv.find {|word| word[0] === "09/06/12"}
+  
+  erb :index
+end
+
+__END__
+
+@@ layout
+  <html>
+    <head>
+      <title>SAT Word of the Day for <%= @word [0]%></title>
+    </head>
+    <body>
+      <%= yield %>
+    </body>
+  </html>
+  
+@@ index
+  <h1><%= @word[1] %></h1>
+  <p>(<em><%= @word[2] %></em>) &rarr; <%= @word[3] %></p>
+  <p><em><%= @word[4] %></em></p>
