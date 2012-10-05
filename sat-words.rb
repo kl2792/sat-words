8ce11dd61b693edc51c7d031888b939121f046f7
Steve Kinney <hello@stevekinney.net>
2012-10-05 12:09:21 -0400

    Fix up JavaScript

--- sat-words.rb
+++ sat-words.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'csv'
+require 'json'
 
 get '/' do
   csv = CSV.open('words.csv')
@@ -10,36 +11,50 @@ get '/' do
   "There is no SAT Word of the Day today."
 end
 
-get '/js' do
+get '/sat-word.js' do
   content_type 'text/javascript', :charset => 'utf-8'
   csv = CSV.open('words.csv')
   today = Date.today.strftime('%m/%d/%y')
   @word = csv.find {|word| word[0] === today}
   
-  
+  erb :js, :layout => false
 end
 
 __END__
 
 @@ layout
-  <html>
-    <head>
-      <title>SAT Word of the Day for <%= @word [0]%></title>
-      <style>
-        body {font-family: Helvetica, Arial, "MS Trebuchet", sans-serif;}
-      </style>
-    </head>
-    <body>
-      <%= yield %>
-    </body>
-  </html>
+<html>
+  <head>
+    <title>SAT Word of the Day for <%= @word [0]%></title>
+    <style>
+      body {font-family: Helvetica, Arial, "MS Trebuchet", sans-serif;}
+    </style>
+  </head>
+  <body>
+    <%= yield %>
+  </body>
+</html>
   
 @@ index
-  <h1><%= @word[1] %></h1>
-  <p>(<em><%= @word[2] %></em>) &rarr; <%= @word[3] %></p>
-  <p><em><%= @word[4] %></em></p>
+<h1><%= @word[1] %></h1>
+<p>(<em><%= @word[2] %></em>) &rarr; <%= @word[3] %></p>
+<p><em><%= @word[4] %></em></p>
   
 @@ js
-  (function(){
-    return <%= @word.to_json %>
-  })()
+(function satWord($){
+  var word, formattedWord;
+  word = {
+    date: "<%= @word[0] %>",
+    word: "<%= @word[1] %>",
+    partOfSpeech: "<%= @word[2] %>",
+    definition: "<%= @word[3] %>",
+    sentence: "<%= @word[4] %>"
+  }
+  
+  formattedWord = "<h2>SAT Word of the Day</h2>\n";
+  formattedWord += "<p>(<em>" + word.partOfSpeech + "</em>) &rarr; ";
+  formattedWord += word.definition + "</p>\n";
+  formattedWord += "<p>" + word.sentence + "</p>";
+  
+  $(body).prepend(formattedWord);
+})($)
